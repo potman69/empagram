678bb70d7b1dbe0c79e05afb4a8c0bfffb704890
potman69 <anton@twk.co.za>
2017-06-14 08:58:54 +0200

    node up and running

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ const flash = require('express-flash');
 const LightRoutes = require('./light-routes');
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 // parse application/json
 app.use(bodyParser.json());
 app.use(express.static('public'));
@@ -24,15 +26,103 @@ app.use(session({
 app.use(flash());
 
 //setup handlebars
-app.engine('hbs', expressHandlebars({defaultLayout: 'main'}));
+app.engine('hbs', expressHandlebars({
+  defaultLayout: 'main'
+}));
 app.set('view engine', 'hbs');
 
 const lightRoutes = LightRoutes();
 
-app.get('/', lightRoutes.index);
-app.post('/light', lightRoutes.light);
+app.get('/login/:username', function(req, res) {
+  req.session.username = req.params.username;
+  res.redirect('/')
+});
 
-var port = process.env.port || 3007;
-http.listen(port, function(){
-    console.log('running at port :' , port)
+app.get('/who', function(req, res) {
+  //req.session.username = req.params.username;
+  res.send(req.session.username);
+});
+
+
+//app.post('/light', lightRoutes.light);
+
+app.get('/', function(req, res) {
+  //req.session.username = req.params.username;
+  res.render('index', {
+    username: req.session.username
+  });
+});
+
+
+var stories = [{
+    id: "1",
+    heading: "",
+    content: "",
+    likeCount: 0
+  },
+  {
+    id: "2",
+    heading: "",
+    content: "",
+    likeCount: 0
+  },
+
+];
+
+var userLikes = {
+};
+
+app.get('/stories.html', function(req, res) {
+  //req.session.username = req.params.username;
+  res.render('stories', {
+    username: req.session.username,
+    stories: stories
+  });
+});
+
+app.get('/likes', function(req, res){
+  res.send({
+    stories,
+    userLikes
+  });
+})
+
+app.get('/likeOrUnlike/:storyId', function(req, res) {
+
+  var username = req.session.username;
+
+  if (!username) {
+    return res.send('you should be logged in')
+  }
+
+  var myLikes = userLikes[username];
+  var storyId = req.params.storyId;
+
+  var currentStory = stories.find(function(story) {
+    return story.id === storyId;
+  })
+
+  if (myLikes === undefined) {
+    userLikes[username] = {}
+  }
+  myLikes = userLikes[username];
+
+  var userStoryInterAction = myLikes[storyId];
+
+  //liking
+  if (userStoryInterAction === undefined || !userStoryInterAction) {
+    myLikes[storyId] = true;
+    currentStory.likeCount++;
+  } else { //unliking
+    myLikes[storyId] = false;
+    currentStory.likeCount--;
+  }
+
+  res.redirect('/stories.html')
+
+});
+
+var port = process.env.PORT || 3007;
+http.listen(port, function() {
+  console.log('running at port :', port)
 });
